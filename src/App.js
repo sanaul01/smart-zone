edb6aff2b185d83ac05d870683de9b57b2e56d0c
sanaul01 <mdsanaulislam154@gmail.com>
2022-04-26 17:30:54 +0600

    install useable element

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h2>This is my e-comarch web-site</h2>
+      <Navbar/>
+      <Home/>
     </div>
   );
 }
